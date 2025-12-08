2be237756ebbde036debb36ad7a925052cd645d0
ylzcoding <93677832+ylzcoding@users.noreply.github.com>
2025-12-07 20:54:31 -0500

    Update auto_find_start.R

--- R/auto_find_start.R
+++ R/auto_find_start.R
@@ -25,9 +25,8 @@ initialize_adaptive <- function(X, y,
                                 iter_selection = 5000,
                                 candidates = list(
                                   horseshoe = list(a = 0.5, b = 0.5),
-                                  sb = list(a = 1.0, b = 0.5),
-                                  student_t = list(a = 10.0, b = 10.0),
-                                  normal_gamma = list(a = 0.5, b = 20.0)
+                                  normal = list(a = 10.0, b = 10.0),
+                                  normal_gamma = list(a = 0.5, b = 10.0)
                                 ),
                                 woodbury = TRUE, IS_period = 1,
                                 delta1 = 1e-6, delta2 = 1e-3, delta3 = 1e-3,
